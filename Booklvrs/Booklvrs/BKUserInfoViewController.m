382e1ecc770ab1b914ad979050036f0b6f9cbfab
Yonatan Kogan <yonatan@romotive.com>
2013-05-18 15:11:03 -0700

    Stopped ignoring the frameworks folder for now b/c it's convenient.
    User data is now submitted to Parse! Next step is getting the user's location.

--- Booklvrs/Booklvrs/BKUserInfoViewController.m
+++ Booklvrs/Booklvrs/BKUserInfoViewController.m
@@ -8,10 +8,14 @@
 
 #import "BKUserInfoViewController.h"
 #import <QuartzCore/QuartzCore.h>
+#import <Parse/Parse.h>
 
 CGFloat kPaddingFromEdge = 10.0f;
 
 @interface BKUserInfoViewController ()
+
+@property (strong, nonatomic) UITextField *goodReadsUsername;
+@property (strong, nonatomic) UITextField *lookingFor;
 
 @end
 
@@ -32,11 +36,10 @@ CGFloat kPaddingFromEdge = 10.0f;
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"booklvrs_bkground.jpg"]];    
     CGFloat navHeight = self.navigationController.navigationBar.frame.size.height;
     UIView *containerView = [[UIView alloc] initWithFrame:CGRectMake(20.0,
-                                                                    60.0,
+                                                                    10.0,
                                                                     self.view.frame.size.width - 40.0,
                                                                     self.view.frame.size.height - navHeight - 120.0)];
     containerView.userInteractionEnabled = YES;
-//    containerView.backgroundColor = [UIColor blueColor];
 
     UILabel *goodReadsUsernameLabel = [[UILabel alloc] initWithFrame:CGRectMake(
                                                                         kPaddingFromEdge,
@@ -47,33 +50,73 @@ CGFloat kPaddingFromEdge = 10.0f;
     goodReadsUsernameLabel.text = @"Goodreads Username";
     goodReadsUsernameLabel.textColor = [UIColor whiteColor];
     
-    UITextField *goodReadsUsername = [[UITextField alloc] initWithFrame:CGRectMake(
+    self.goodReadsUsername = [[UITextField alloc] initWithFrame:CGRectMake(
                                                                                    kPaddingFromEdge,
                                                                                    goodReadsUsernameLabel.frame.size.height+ kPaddingFromEdge,
                                                                                    containerView.frame.size.width - 2*kPaddingFromEdge,
                                                                                    40)];
-    goodReadsUsername.borderStyle = UITextBorderStyleRoundedRect;
-//    goodReadsUsername.backgroundColor = [UIColor redColor];
+    self.goodReadsUsername.borderStyle = UITextBorderStyleRoundedRect;
 
+    UILabel *lookingForLabel = [[UILabel alloc] initWithFrame:CGRectMake(kPaddingFromEdge,
+                                                                        self.goodReadsUsername.frame.origin.y + self.goodReadsUsername.frame.size.height + kPaddingFromEdge,
+                                                                        containerView.frame.size.width - 2*kPaddingFromEdge,
+                                                                         30.0)];
+    
+    lookingForLabel.backgroundColor = [UIColor clearColor];
+    lookingForLabel.text = @"What are you looking for?";
+    lookingForLabel.textColor = [UIColor whiteColor];
+    
+    self.lookingFor = [[UITextField alloc] initWithFrame:CGRectMake(kPaddingFromEdge,
+                                                                    lookingForLabel.frame.origin.y + lookingForLabel.frame.size.height + kPaddingFromEdge,
+                                                                    containerView.frame.size.width - 2*kPaddingFromEdge,
+                                                                    40.0)];
+    self.lookingFor.borderStyle = UITextBorderStyleRoundedRect;
+    
     UIButton *submitBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     submitBtn.frame = CGRectMake(kPaddingFromEdge,
-                                 goodReadsUsername.frame.origin.y +
-                                 goodReadsUsername.frame.size.height  + kPaddingFromEdge,
+                                 self.lookingFor.frame.origin.y +
+                                 self.lookingFor.frame.size.height  + kPaddingFromEdge,
                                  containerView.frame.size.width - 2*kPaddingFromEdge,
                                  40);
+    
     [submitBtn setTitle:@"Submit" forState:UIControlStateNormal];
     [submitBtn addTarget:self action:@selector(submitBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
     
+    UITapGestureRecognizer *dismissKeyboardRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard:)];
+    
+    [self.view addGestureRecognizer:dismissKeyboardRecognizer];
     [containerView addSubview:goodReadsUsernameLabel];
-    [containerView addSubview:goodReadsUsername];
+    [containerView addSubview:self.goodReadsUsername];
+    [containerView addSubview:lookingForLabel];
+    [containerView addSubview:self.lookingFor];
     [containerView addSubview:submitBtn];
     
     [self.view addSubview:containerView];
     
+    
+    
 }
 
 - (void)submitBtnTapped:(id)sender {
-    NSLog(@"got tap");
+    [self.goodReadsUsername resignFirstResponder];
+    [self.lookingFor resignFirstResponder];
+    NSString *goodReadsUsername = self.goodReadsUsername.text;
+    NSString *lookingFor = self.lookingFor.text;
+
+    if (goodReadsUsername.length < 1 || lookingFor.length < 1) {
+        UIAlertView *emptyField = [[UIAlertView alloc] initWithTitle:@"Missing Info" message:@"Please fill out all text fields" delegate:self cancelButtonTitle:@"Fine then" otherButtonTitles: nil];
+        [emptyField show];
+    } else {
+        PFUser *currentUser = [PFUser currentUser];
+        [currentUser setObject:goodReadsUsername forKey:@"GoodReadsUsername" ];
+        [currentUser setObject:lookingFor forKey:@"LookingFor"];
+        [currentUser save];
+    }
+}
+
+- (void)dismissKeyboard:(id)sender {
+    [self.goodReadsUsername resignFirstResponder];
+    [self.lookingFor resignFirstResponder];
 }
 
 @end
