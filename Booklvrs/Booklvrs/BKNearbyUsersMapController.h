ff82e8ce83ed125b067b45af97f61ae4d2f80caa
Zach Fisher <zfisher@mit.edu>
2013-05-19 00:46:43 -0700

    Added maps

---
+++ Booklvrs/Booklvrs/BKNearbyUsersMapController.h
@@ -0,0 +1,16 @@
+//
+//  BKNearbyUsersMapController.h
+//  Booklvrs
+//
+//  Created by Zachary Fisher on 5/18/13.
+//  Copyright (c) 2013 Booklvrs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface BKNearbyUsersMapController : UIViewController
+
+@property (strong) MKMapView *mapView;
+
+@end
